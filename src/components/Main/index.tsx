3a225946eb3d202c8a504faeb6d4f5e2b4350a4a
Pedro Santos <90013568+PedroSantoss-Dev@users.noreply.github.com>
2023-09-07 16:09:13 -0300

    initial settings

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from '../Text';
+import { Container } from './styles';
+
+export const Main = () => {
+  return (
+    <Container>
+      <Text> Main renderizou </Text>
+    </Container>
+  );
+};
